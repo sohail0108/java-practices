5ea36cf25a93ce955482925c6296407919a2f679
sohail0108 <sksohail0108@gmail.com>
2023-12-04 20:31:59 +0530

    'added'

---
+++ src/mypractice/test1.java
@@ -0,0 +1,15 @@
+package mypractice;
+
+public class test1 {
+
+	public test1() {
+		System.out.println("constructor-test1");
+	}
+
+	public static void main(String[] args) {
+		
+		test1 t=new test1();
+		System.out.println("main");
+	}
+
+}
